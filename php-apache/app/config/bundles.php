c042f8e39619e4daed02d2b6618dfd91862857c9
proitcreator <proitcreator@gmail.com>
2023-02-10 09:48:32 +0200

    composer require lexik/jwt-authentication-bundle

--- php-apache/app/config/bundles.php
+++ php-apache/app/config/bundles.php
@@ -6,4 +6,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
