45c14dd8f7df2cecedb5e6e6c917446dc269403e
proitcreator <proitcreator@gmail.com>
2023-02-10 09:26:15 +0200

    composer require symfony/maker-bundle --dev

--- php-apache/app/config/bundles.php
+++ php-apache/app/config/bundles.php
@@ -3,4 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
