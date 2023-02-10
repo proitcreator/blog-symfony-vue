2507b4f05ed3b5a0caa464e9b283f795352e9c03
proitcreator <proitcreator@gmail.com>
2023-02-10 10:06:46 +0200

    composer require api

--- php-apache/app/config/bundles.php
+++ php-apache/app/config/bundles.php
@@ -7,4 +7,7 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
 ];
